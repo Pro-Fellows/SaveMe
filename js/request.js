7d0c24b8b15335bc81ded71a093ab119944b6abe
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-06-13 21:33:05 +0300

    added the localStorage functionalty between the donation and the request pages

--- js/request.js
+++ js/request.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let formEl = document.getElementById('requestForm');
+let cardsCont = document.getElementById('cardsContainer');
+
 RequestBlood.allFields = [];
 
-function RequestBlood(name, age, gender, city, phoneNumber, bloodType, unitNumber, frequency, caseStatus){
+function RequestBlood(name, age, gender, city, phoneNumber, bloodType, unitNumber, frequency, caseStatus) {
   this.name = name;
   this.age = age;
   this.gender = gender;
@@ -24,9 +26,9 @@ let arrayDonors = grabDonors();
 
 
 
-formEl.addEventListener('submit',handleSubmitting);
+formEl.addEventListener('submit', handleSubmitting);
 
-function handleSubmitting(event){
+function handleSubmitting(event) {
 
   event.preventDefault();
 
@@ -42,19 +44,78 @@ function handleSubmitting(event){
 
   new RequestBlood(newName, newAge, newGender, newCity, newNumber, newType, newUnits, newFreq, newCase);
 
+  getDonorBlood(newType);
   getData();
+  // available();
 }
 
 
+let extractedDonorsArray = [];
+
+for (let m = 0; m < arrayDonors.length; m++) {
+  let obj = { 'name': arrayDonors[m].name, 'bloodType': arrayDonors[m].bloodType, 'city': arrayDonors[m].city, 'phone': arrayDonors[m].phone };
+  extractedDonorsArray.push(obj);
+}
+
+console.log(extractedDonorsArray);
 
 // Name , BT , Phone Number , City
 
-function available(){
+
+// function available(x){
+
+//   let liEl = document.createElement('li');
+//   ulEl.appendChild(liEl);
+//   liEl.textContent = extractedDonorsArray[0].name ;
+
+// }
 
 
 
+
+
+
+function getDonorBlood(newType) {
+
+  if (newType === 'O-') {
+    let oNegative = extractedDonorsArray.filter(a => a.bloodType === 'O-');
+    // console.log(oNegative);
+
+    for(let i=0;i<oNegative.length;i++){
+
+      let name = oNegative[i].name;
+      let bloodType = oNegative[i].bloodType;
+      let city = oNegative[i].city;
+      let phone = oNegative[i].phone;
+
+      let divEl = document.createElement('div');
+      divEl.setAttribute('class','card');
+      cardsCont.appendChild(divEl);
+
+      let h1El = document.createElement('h1');
+      divEl.appendChild(h1El);
+      h1El.textContent = `The donar name is: ${name}`;
+
+      let h2El = document.createElement('h2');
+      divEl.appendChild(h2El);
+      h2El.textContent = `The blood type is: ${bloodType}`;
+
+      h2El = document.createElement('h2');
+      divEl.appendChild(h2El);
+      h2El.textContent = `The donar lives in: ${city}`;
+
+      h2El = document.createElement('h2');
+      divEl.appendChild(h2El);
+      h2El.textContent = `The phone number is: ${phone}`;
+
+    }
+
+
+  } else if (newType === 'O+') {
+    let oPostive = extractedDonorsArray.filter(a => a.bloodType === 'O-' || a.bloodType === 'O+');
+    console.log(oPostive);
+  }
 }
-
 
 
 
@@ -70,7 +131,7 @@ function available(){
 
 // beyond this is for storage
 
-function setData(){
+function setData() {
 
   let stringArray = JSON.stringify(RequestBlood.allFields);
 
@@ -79,11 +140,11 @@ function setData(){
 }
 
 
-function getData(){
+function getData() {
 
   let values = localStorage.getItem('Data');
 
-  if(values){
+  if (values) {
     RequestBlood.allField = JSON.parse(values);
   }
 }

3b34ecd4a6695cb7c4225996c06eb011757a0366
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-06-14 09:03:54 +0300

    edied some things

--- js/eligibility.js
+++ js/eligibility.js
@@ -1,3 +1,4 @@
+
 'use strict'
 
 // let arr = ['firstques', 'secques', 'thirdques']
@@ -195,6 +196,18 @@ function noSix (){
 }
  
    
-// function 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
 

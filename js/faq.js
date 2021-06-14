45ac936ca0ee904942fb7896ed234b582648fe16
lamisghanayem <ghanayemlamis@gmail.com>
2021-06-14 15:14:03 +0300

    moved enquiry html page to the faq page with writing js to show the form in the faq page

---
+++ js/faq.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+'user strict';
+
+
+// let btn= document.getElementById(showEnquiryForm);
+
+// btn.addEventListener('click', showForm);
+
+// function showForm(event){
+    
+//     event.preventDefault();
+
+
+// }
+
+function showForm() {
+    document.getElementById('EnquiryForm').style.display = 'block';
+}

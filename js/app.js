5c8a4e3716551a1caed587389bb098f26021609a
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-06-14 21:30:23 +0300

    added a divider with dynamic number of donors

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-'user strict'
+'user strict';
 
+let counter = document.getElementById('donorsNumber');
+let arrayOfDonors = grabDonors();
+
+counter.textContent = arrayOfDonors.length;
+
+
+
+
+
+
+function grabDonors() {
+
+    let storedDonArr = JSON.parse(localStorage.getItem('arrayDonors'));
+    return storedDonArr;
+  }

76a007bd9920acc8eed360566f12be0a8b7b2ad4
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-06-15 21:31:50 +0300

    added an animation video to the home page

--- js/app.js
+++ js/app.js
@@ -12,6 +12,18 @@ counter.textContent = arrayOfDonors.length;
 
 function grabDonors() {
 
-    let storedDonArr = JSON.parse(localStorage.getItem('arrayDonors'));
-    return storedDonArr;
-  }
+  let storedDonArr = JSON.parse(localStorage.getItem('arrayDonors'));
+  return storedDonArr;
+}
+
+
+
+let videoElem = document.getElementById('v');
+ScrollTrigger.create({
+  trigger: videoElem,
+  onEnter: () => videoElem.play(),
+  onEnterBack: () => videoElem.play(),
+  onLeave: () => videoElem.pause(),
+  onLeaveBack: () => videoElem.pause(),
+});
+

13e7a970c22312813e3ffee83a390eae16ebf097
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-06-16 21:03:27 +0300

    styling header

---
+++ js/header.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 807 || document.documentElement.scrollTop > 807) {
+    document.getElementById('header').style.backgroundColor = '#144955';
+  } else {
+    document.getElementById('header').style.backgroundColor = 'whitesmoke';
+  }
+}
